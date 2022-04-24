d4372ac54c451c09fc9f93bb779e347827b0a007
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-24 09:16:13 +0300

    Lesson 2. Base class APIView for views

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,8 +1,26 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Women
 from .serializers import WomenSerializer
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
