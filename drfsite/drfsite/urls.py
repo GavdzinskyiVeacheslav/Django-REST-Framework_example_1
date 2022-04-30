5d62bb35815beefd8b142caacfc4f77e895dc1a9
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-30 20:00:48 +0300

    'Lesson_8_Routers_SimpleRouter_and_DefaultRouter'

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,11 +1,27 @@
 
 from django.contrib import admin
 from django.urls import path, include
+
 from women.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,29 +1,42 @@
+
 from django.forms import model_to_dict
 from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
-
-
-# class WomenViewSet(viewsets.ModelViewSet):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
 
 
 class WomenViewSet(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
                    mixins.UpdateModelMixin,
-                    
                    mixins.ListModelMixin,
                    GenericViewSet):
-    queryset = Women.objects.all()
+    #queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+    def get_queryset(self):
+      pk = self.kwargs.get('pk')
 
+      if not pk:
+        return Women.objects.all()[:3]
+
+      return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+      cats = Category.objects.get(pk=pk)
+      return Response({'cats': cats.name})
+
+
+
+# class WomenViewSet(viewsets.ModelViewSet):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
 # class WomenAPIList(generics.ListCreateAPIView):
 #     queryset = Women.objects.all()
