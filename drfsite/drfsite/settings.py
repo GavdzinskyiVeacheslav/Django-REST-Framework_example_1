72bf71dcf346cd4988ce4ce2055aa322e290d329
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-05-01 01:45:57 +0300

    Lesson_13_Pagination

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -128,9 +128,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.LimitOffsetPagination',
+        'PAGE_SIZE' : 2,
+
+        'DEFAULT_RENDERER_CLASSES' : [
+            'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -10,17 +10,26 @@ from rest_framework.viewsets import GenericViewSet
 from .models import Women, Category
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 2
+
 
 class WomenAPIList(generics.ListCreateAPIView):
   queryset = Women.objects.all()
   serializer_class = WomenSerializer
   permission_classes = (IsAuthenticatedOrReadOnly,)
+  pagination_class = WomenAPIListPagination
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
   queryset = Women.objects.all()
   serializer_class = WomenSerializer
   permission_classes = (IsAuthenticated, )
-  authentication_class = (TokenAuthentication, )
+  #authentication_class = (TokenAuthentication, )
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
   queryset = Women.objects.all()
